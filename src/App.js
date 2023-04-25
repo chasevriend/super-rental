66be593bdfd3e4e939200807f29543832c459d4f
NEWUSER <chasevriend84@gmail.com>
2023-04-25 11:13:32 -0700

    new commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './css/App.css';
 import Home from './pages/Home';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
